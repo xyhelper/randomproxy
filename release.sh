2b5c1c2e7bcd09c754fb56fc1913b2119028b939
Li Dong <cnlidong@live.cn>
2023-07-29 21:39:55 +0800

    fix release

---
+++ release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -e
+
+
+gf docker main.go -p -t xyhelper/randomproxy:latest
--- releash.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-go build -o randomproxy
-docker build -t xyhelper/randomproxy:latest .
-docker push xyhelper/randomproxy:latest
